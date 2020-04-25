aa49d6e4133ae299f70460f75a78de9fd2afd243
Sanjeev Sreenath <sreenath.sa@husky.neu.edu>
2020-04-25 18:52:33 -0400

    version 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dockerizeme',
-    version='0.1.4',
+    version='0.2',
     packages=find_packages(),
     py_modules=['dockerizeme'],
     description ='Generate Dockerfile for your project automatically',
